9329ee2b850e66528cf1d2887ca91e9f4f12eba2
Mark Ellis <markellisdev@gmail.com>
2020-10-08 17:56:15 -0500

    implemented shortcut to start_requests method by defining start_urls instead

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -3,13 +3,18 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+#### Actually don't have to use the start_requests function since it's built in. Can just use start_urls
+    # def start_requests(self):
+    #     urls = [
+    #         'http://quotes.toscrape.com/page/1/',
+    #         'http://quotes.toscrape.com/page/2/'
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/'
+    ]
 
     def parse(self, response):
         page = response.url.split("/")[-2]
