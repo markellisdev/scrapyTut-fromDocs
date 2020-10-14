6bcfdebebbce5c3bdcb9f7b4757150f04ffd0e38
Mark Ellis <markellisdev@gmail.com>
2020-10-14 16:56:00 -0500

    scraping multiple pages now using built in feed exports to output json or jl (json lines)

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.exporters import JsonLinesItemExporter
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -16,9 +17,24 @@ class QuotesSpider(scrapy.Spider):
         'http://quotes.toscrape.com/page/2/'
     ]
 
+#### Original parse to just get the entire page
+    # def parse(self, response):
+    #     page = response.url.split("/")[-2]
+    #     filename = 'quotes-%s.html' % page
+    #     with open(filename, 'wb') as f:
+    #         f.write(response.body)
+    #     self.log('Saved file %s' % filename)
+
+#### Parse to actually gather targeted info
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").get(),
+                'author': quote.css("small.author::text").get(),
+                'tags': quote.css("div.tags a.tag::text").getall()
+            }
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
