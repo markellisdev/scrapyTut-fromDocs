cfc1255dd301f989680a0fa5222a5361468b996b
Mark Ellis <markellisdev@gmail.com>
2021-01-11 16:58:06 -0600

    expanding quotes spider added additional to scrape author info adding Item and Loader next, to implement in other project

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -6,8 +6,23 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exporters import JsonLinesItemExporter
 
 
 class TutorialPipeline:
     def process_item(self, item, spider):
+        return item
+
+class JsonPipeline(object):
+    def __init__(self):
+        self.file = open("news_2021.json", 'wb')
+        self.exporter = JsonLinesItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -13,8 +13,7 @@ class QuotesSpider(scrapy.Spider):
     #     for url in urls:
     #         yield scrapy.Request(url=url, callback=self.parse)
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/'
+        'http://quotes.toscrape.com'
     ]
 
 #### Original parse to just get the entire page
@@ -27,14 +26,32 @@ class QuotesSpider(scrapy.Spider):
 
 #### Parse to actually gather targeted info
     def parse(self, response):
-        for quote in response.css("div.quote"):
+        quotes = response.css("div.quote")
+        for quote in quotes:
             yield {
                 'text': quote.css("span.text::text").get(),
                 'author': quote.css("small.author::text").get(),
                 'tags': quote.css("div.tags a.tag::text").getall()
             }
 
-        next_page = response.css("li.next a::attr(href)").get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            author_url = quote.css('.author + a::attr(href)').get()
+            self.logger.info('get author page url')
+            # go to the author page
+            yield response.follow(author_url, callback=self.parse_author)
+
+        # next_page = response.css("li.next a::attr(href)").get()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
+
+        # Replaced the above with........
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
+    
+    def parse(self, response):
+        yield {
+            'author_name': response.css('.author-title::text').get(),
+            'author_birthday': response.css('.author-born-date::text').get(),
+            'author_bornlocation': response.css('.author-born-location::text').get(),
+            'author_bio': response.css('.author-description::text').get()
+        }
